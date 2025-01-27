9cc8b7a34b73595f67fe3caaa0ee3c4955ce9940
jimmyberg <jimmyberg.kim@gmail.com>
2025-01-27 20:24:45 +0900

    fix: external-url 프로퍼티 관련 configuration 클래스 수정

--- src/main/kotlin/com/konai/fxs/infra/config/ExternalUrlProperties.kt
+++ src/main/kotlin/com/konai/fxs/infra/config/ExternalUrlProperties.kt
@@ -1,28 +1,18 @@
 package com.konai.fxs.infra.config
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
-import com.fasterxml.jackson.module.kotlin.kotlinModule
-import com.fasterxml.jackson.module.kotlin.readValue
 import com.konai.fxs.common.EMPTY
 import com.konai.fxs.common.enumerate.ExternalComponent
 import com.konai.fxs.infra.error.ErrorCode
 import com.konai.fxs.infra.error.exception.InternalServiceException
-import org.springframework.core.io.ClassPathResource
-import org.springframework.stereotype.Component
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
 
-@Component
+@ConfigurationProperties(prefix = "external-url")
+@Configuration
 class ExternalUrlProperties {
 
-    private val properties: Map<String, ExternalUrlProperty> = getExternalUrlProperties()
-
-    private fun getExternalUrlProperties(): Map<String, ExternalUrlProperty> {
-        val objectMapper = ObjectMapper(YAMLFactory()).registerModule(kotlinModule())
-        val resource = ClassPathResource("application-external-url.yml")
-        val externalUrlMap: Map<String, Map<String, ExternalUrlProperty>> = objectMapper.readValue(resource.inputStream)
-        return externalUrlMap["external-url"] ?: emptyMap()
-    }
+    lateinit var properties: Map<String, ExternalUrlProperty>
 
     fun getProperty(component: ExternalComponent): ExternalUrlProperty {
         return this.properties[component.getPropertyName()] ?: throw InternalServiceException(ErrorCode.EXTERNAL_URL_PROPERTY_NOT_DEFINED)
