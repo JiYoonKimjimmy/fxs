9da27df3e55fc4a45e17e00b826a62b0224d52d7
jimmyberg <jimmyberg.kim@gmail.com>
2025-02-17 12:17:46 +0900

    feat: TestConfiguration 클래스 개선

--- src/test/kotlin/com/konai/fxs/testsupport/redis/EmbeddedRedisConfig.kt
+++ src/test/kotlin/com/konai/fxs/testsupport/redis/EmbeddedRedisConfig.kt
@@ -5,7 +5,9 @@ import jakarta.annotation.PreDestroy
 import org.redisson.api.RedissonClient
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Profile
 
+@Profile("test")
 @TestConfiguration
 class EmbeddedRedisConfig {
 
