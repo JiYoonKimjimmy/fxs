1f92cd4054dc0e8eef9ef6ac79df5b76e3b27d1c
jimmyberg <jimmyberg.kim@gmail.com>
2025-02-17 12:43:50 +0900

    feat: ObjectMapper bean 생성 개선

--- src/main/kotlin/com/konai/fxs/infra/config/ApplicationConfig.kt
+++ src/main/kotlin/com/konai/fxs/infra/config/ApplicationConfig.kt
@@ -1,6 +1,7 @@
 package com.konai.fxs.infra.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.kotlinModule
 import com.konasl.commonlib.logging.filter.EnableLoggingFilter
@@ -28,7 +29,7 @@ class ApplicationConfig {
     @Primary
     @Bean
     fun objectMapper(): ObjectMapper {
-        return jacksonObjectMapper().registerModule(kotlinModule())
+        return jacksonObjectMapper().registerModules(kotlinModule(), JavaTimeModule())
     }
 
 }
